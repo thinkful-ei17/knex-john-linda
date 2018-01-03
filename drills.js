dcc378602c0ca0d270b092fa3195a467cc47da24
Linda Foo <linda.foo.dev@gmail.com>
2018-01-03 13:42:56 -0800

    finished treeize challenge

--- drills.js
+++ drills.js
@@ -162,31 +162,50 @@ process.stdout.write('\x1Bc');
 // });
 // console.log(hydrated);
 
+// knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+//   .from('restaurants')
+//   .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
+//   .orderBy('date', 'desc')
+//   .limit(10)
+//   .then(results => {
+
+//     const hydrated = {};
+//     results.forEach(row => {
+//       if ( !(row.id in hydrated) ) {
+//         hydrated[row.id] = {
+//           name: row.name,
+//           cuisine: row.cuisine,
+//           borough: row.borough,
+//           grades: []
+//         };
+//       }
+//       hydrated[row.id].grades.push({
+//         gradeId: row.gradeId,
+//         grade: row.grade,
+//         score: row.score
+//       });
+//     });
+//     console.log(JSON.stringify(hydrated, null, 4));
+//   });
+
+//TREEIZE CHALLENGE --------------------------------------
+
+var Treeize   = require('treeize');
+const restaurants   = new Treeize();
+
 knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
   .from('restaurants')
   .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
   .orderBy('date', 'desc')
   .limit(10)
   .then(results => {
-
-    const hydrated = {};
-    results.forEach(row => {
-      if ( !(row.id in hydrated) ) {
-        hydrated[row.id] = {
-          name: row.name,
-          cuisine: row.cuisine,
-          borough: row.borough,
-          grades: []
-        };
-      }
-      hydrated[row.id].grades.push({
-        gradeId: row.gradeId,
-        grade: row.grade,
-        score: row.score
-      });
-    });
-    console.log(JSON.stringify(hydrated, null, 4));
+    restaurants.grow(results);
+    console.log(JSON.stringify(restaurants.getData(), null, 4));
   });
+
+//TREEIZE CHALLENGE END--------------------------------------
+
+
 
 // Destroy the connection pool
 knex.destroy().then(() => {
