94007d0bcc684cdff1790a4ca8b607d7d5368f8c
John Castillo <JohnluisCastillo@gmail.com>
2018-01-03 12:05:54 -0500

    fixed

--- drills.js
+++ drills.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const { DATABASE } = require('./config');
-const knex = require('knex')(DATABASE);
-
+const knex = require('knex')({
+  client: 'pg',
+    connection: 'postgres://rgxxvnxt:PFi5DU8d1lrU-j3ZaoYBoEJcsacY64vY@baasu.db.elephantsql.com:5432/rgxxvnxt'
+});
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
