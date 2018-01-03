ccd2ba8c6c5bc9c76fb8227f4cb5b27d243b5040
Linda Foo <linda.foo.dev@gmail.com>
2018-01-03 14:24:34 -0800

    edited post based on chris comments

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 const { DATABASE, PORT } = require('./config');
 const knex = require('knex')({
@@ -10,23 +12,31 @@ const knex = require('knex')({
 
 const app = express();
 
-app.get('/restaurants', (req, res) => {
-  knex.select('id', 'name', 'cuisine', 'borough')
-    .from('restaurants')
-    .limit(10)
-    .then(results => res.json(results));
-});
+// app.get('/restaurants', (req, res) => {
+//   knex.select('id', 'name', 'cuisine', 'borough')
+//     .from('restaurants')
+//     .limit(10)
+//     .then(results => res.json(results));
+// });
 
 // ADD ANSWERS HERE
-app.get('/restaurants/:id', (req, res) => {
-  knex.first('restaurants.id', 'name', 'cuisine', 'borough','grades.id as grades_id' , 'grade', 'date as inspectionDate', 'score')
-    .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
-    .from('restaurants')
-    .where('restaurants.id', req.params.id)
-    .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
-    .orderBy('date', 'desc')
+// app.get('/restaurants/:id', (req, res) => {
+//   knex.first('restaurants.id', 'name', 'cuisine', 'borough','grades.id as grades_id' , 'grade', 'date as inspectionDate', 'score')
+//     .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
+//     .from('restaurants')
+//     .where('restaurants.id', req.params.id)
+//     .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
+//     .orderBy('date', 'desc')
+//     .then(results => res.json(results));
+// });
+
+app.post('/restaurants/', jsonParser, (req, res) => {
+  knex
+    .insert([{name: req.body.name, borough: req.body.borough, cuisine: req.body.cuisine}],'id').into('restaurants')
     .then(results => res.json(results));
 });
 
+//in express - 3 ways to send info; what are 3 ways to receive
+//params is from paramatized routing ex. /restaurants/:id
 
 app.listen(PORT);
