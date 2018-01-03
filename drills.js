3a22e5e41829608d773498da08b98d87855aa0f1
Linda Foo <linda.foo.dev@gmail.com>
2018-01-03 13:27:26 -0800

    hydrate challenge completed

--- drills.js
+++ drills.js
@@ -107,6 +107,87 @@ process.stdout.write('\x1Bc');
 //   .del()
 //   .then(results => console.log(results));
 
+
+//hydrate/dehydrate exercises
+
+// [
+//   {
+//     '2': {
+//       'id': 2,
+//       'name': 'John Doe',
+//       'age': 34,
+//       'pets': [
+//         {
+//           'name': 'Rex',
+//           'type': 'dog'
+//         },
+//         {
+//           'name': 'Rex',
+//           'type': 'dog'
+//         }
+//       ]
+//     },
+//     '3': {
+//       'id': 3,
+//       'name': 'Mary Jane',
+//       'age': 19,
+//       'pets': [
+//         {
+//           'name': 'Mittens',
+//           'type': 'kitten'
+//         },
+//         {
+//           'name': 'Fluffy',
+//           'type': 'cat'
+//         }
+//       ]
+//     }
+//   }
+// ];
+
+// const hydrated = {};
+// people.forEach(row => {
+//   if ( !(row.id in hydrated) ) {
+//     hydrated[row.id] = {
+//       id: row.id,
+//       name: row.name,
+//       age: row.age,
+//       pets: []
+//     };
+//   }
+//   hydrated[row.id].pets.push({
+//     name: row.petName,
+//     type: row.petType,
+//   });
+// });
+// console.log(hydrated);
+
+knex.select('restaurants.id', 'name', 'cuisine', 'borough', 'grades.id as gradeId', 'grade', 'score')
+  .from('restaurants')
+  .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')
+  .orderBy('date', 'desc')
+  .limit(10)
+  .then(results => {
+
+    const hydrated = {};
+    results.forEach(row => {
+      if ( !(row.id in hydrated) ) {
+        hydrated[row.id] = {
+          name: row.name,
+          cuisine: row.cuisine,
+          borough: row.borough,
+          grades: []
+        };
+      }
+      hydrated[row.id].grades.push({
+        gradeId: row.gradeId,
+        grade: row.grade,
+        score: row.score
+      });
+    });
+    console.log(JSON.stringify(hydrated, null, 4));
+  });
+
 // Destroy the connection pool
 knex.destroy().then(() => {
   console.log('database connection closed');
--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.get('/restaurants', (req, res) => {
 
 // ADD ANSWERS HERE
 app.get('/restaurants/:id', (req, res) => {
-knex.first('restaurants.id', 'name', 'cuisine', 'borough','grades.id as grades_id' , 'grade', 'date as inspectionDate', 'score')
-  .select(knex.raw("CONCAT(address_building_number, ' ', address_street, ' ', address_zipcode ) as address"))
-  .from('restaurants')
-  .where('restaurants.id', req.params.id)
-  .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
-  .orderBy('date', 'desc')
-  .then(results => res.json(results));
+  knex.first('restaurants.id', 'name', 'cuisine', 'borough','grades.id as grades_id' , 'grade', 'date as inspectionDate', 'score')
+    .select(knex.raw('CONCAT(address_building_number, \' \', address_street, \' \', address_zipcode ) as address'))
+    .from('restaurants')
+    .where('restaurants.id', req.params.id)
+    .innerJoin('grades', 'restaurants.id', 'grades.restaurant_id')    
+    .orderBy('date', 'desc')
+    .then(results => res.json(results));
 });
 
 
